8ebffa1e8cad4dc183a6d2da995cadf393dae65a
ma15ruch <ruchi.maheshwari2010@yahoo.com>
2014-09-27 18:22:57 -0700

    Create Truck.java

---
+++ Truck.java
@@ -0,0 +1,39 @@
+import zen.core.Zen;
+
+public class Truck {
+	int x;
+	int speed;
+	int lane;
+
+	public boolean isKilling(Frog target){
+		int y = 180 + 50 * lane;
+
+		if (Math.abs(target.x - (x + 80)) < 30 && Math.abs(target.y - y) < 30){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	// Actions
+	public void setup() {
+		x = Zen.getRandomNumber(-500, -100);
+		speed = Zen.getRandomNumber(3, 8);
+		lane = Zen.getRandomNumber(0, 5);
+	}
+
+	public void draw() {
+		int y = 180 + 50 * lane;
+		Zen.setColor("orange");
+		Zen.fillRect(x, y, 80, 40);
+	}
+
+	public void move() {
+		x = x + speed;
+		if (x > 800) {
+			setup();
+		}
+	}
+
+}
