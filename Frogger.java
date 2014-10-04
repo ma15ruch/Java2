4c6b0fcba61b0bb2bc75f03548b5c1d7fdd4be2e
ma15ruch <ruchi.maheshwari2010@yahoo.com>
2014-10-04 16:54:17 -0700

    Update Frogger.java

--- Frogger.java
+++ Frogger.java
@@ -9,10 +9,13 @@ public class Frogger {
 
 		Frog f = new Frog();
 		f.setup();
+
 		ArrayList<Truck> list = new ArrayList<Truck>();
+		ArrayList<Car> list2 = new ArrayList<Car>();
+		ArrayList<Log> list3 = new ArrayList<Log>();
 
 		int count = 0;
-		while (count < 50) {
+		while (count < 20) {
 			Truck t = new Truck();
 			t.setup();
 			list.add(t);
@@ -20,29 +23,80 @@ public class Frogger {
 			count = count + 1;
 		}
 
+		int count2 = 0;
+		while (count2 < 25) {
+			Car c = new Car();
+			c.setup();
+			list2.add(c);
+
+			count2 = count2 + 1;
+		}
+
+		int count3 = 0;
+		while (count3 < 25) {
+			Log l = new Log();
+			l.setup();
+			list3.add(l);
+
+			count3 = count3 + 1;
+		}
+		
 		while (true) {
 			// draw stuff
 			drawStuff();
 			f.draw();
 			f.move();
+
 			for (Truck t : list) {
 				t.draw();
 				t.move();
-				if ((t.isKilling(f))){
+				if ((t.isKilling(f))) {
 					f.setup();
-					for (Truck t1 : list){
+					/*for (Truck t1 : list) {
 						t1.setup();
 					}
+					for (Car c1 : list2) {
+						c1.setup();
+					}*/
 				}
+			}
+
+			for (Car c : list2) {
+				c.draw();
+				c.move();
+				if ((c.isKilling(f))) {
+					f.setup();
+					/*for (Car c1 : list2) {
+						c1.setup();
+					}
+					for (Truck t1 : list) {
+						t1.setup();
+					}*/
+				}
+			}
+			
+			for (Log l : list3) {
+				l.draw();
+				l.move();
+				//while ((l.isFrogOnLog(f))) {
+					// f has to move with log
+					//f.x = f.x + l.speed;
+				//}
+				//if ((!l.isFrogOnLog(f))) {
+					// f has to move with log
+					//f.setup();
+				//}
 			}
 
 			Zen.buffer(33);
 		}
 	}
 
-	private static void drawFrog() {
-
-	}
+	/*
+	 * private static void drawFrog() {
+	 * 
+	 * }
+	 */
 
 	private static void drawStuff() {
 		Zen.setBackground("gray");
