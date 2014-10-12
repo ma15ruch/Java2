b1c3d0913971ca1a2e7a56a31fc02a0fb5db341a
ma15ruch <ruchi.maheshwari2010@yahoo.com>
2014-10-11 18:27:07 -0700

    Update Brick.java

--- Brick.java
+++ Brick.java
@@ -6,6 +6,13 @@ public class Brick {
 
 	int x;
 	int y;
+	int health;
+	
+	public Brick() {
+		x = 200;
+		y = 200;
+		health = 1;
+	}
 
 	public Brick(int newX, int newY) {
 		x = newX;
@@ -15,5 +22,19 @@ public class Brick {
 	public void draw() {
 		Zen.setColor("purple");
 		Zen.fillRect(x - 20, y - 10, 40, 20);
+	}
+
+	public void move(){
+		
+	}
+	
+	public boolean isTouching(Ball b) {
+		if (Math.abs(x - b.x) < 25 && Math.abs(y - b.y) < 16) {
+
+			health--;
+			b.dx = Zen.getRandomNumber(-8, 8);
+			return true;
+		}
+		return false;
 	}
 }
